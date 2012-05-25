4ceb0763cf50c76725712f53a68f35a65af42b7d
João Paulo <jpjcjbr@gmail.com>
2012-05-25 12:39:57 -0300

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  before_filter :authenticate
+  # before_filter :authenticate
   helper_method :current_user
   helper_method :admin_does_not_exist
 
