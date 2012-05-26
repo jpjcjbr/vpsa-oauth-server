8ce369f3325e0f69212791ae76370036854d47a6
João Paulo <jpjcjbr@gmail.com>
2012-05-26 01:28:30 -0300

    fim debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,8 @@ class ApplicationController < ActionController::Base
       @body = HashWithIndifferentAccess.new(JSON.parse(request.body.read.to_s))
     end
 
-    def authenticate
-      print '<<<<<<<<<<<<< format' << request.format
-      
+    def authenticate      
       if api_request
-        print '<<<<<<<< api request'
         # oauth_authorized   # uncomment to make all json API protected
       else
         session_auth
@@ -40,7 +37,6 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.criteria.id(session[:user_id]).first if session[:user_id]
       unless current_user
         session[:back] = request.url 
-        print '<<<<<<<<<<<<<<<<<<<<<<< redirecionou a bagaca'
         redirect_to(log_in_path) and return false
       end
       return @current_user
@@ -51,15 +47,12 @@ class ApplicationController < ActionController::Base
     end
 
     def oauth_authorized
-      print '<<<<<<<<<<<<<<<<<<<<<<<<< oauth_authorized'
+
       action = params[:controller] + "/" + params[:action]
-      print '<<<<<<<<<<<<<<<<<<<<<<<<< action ' << action
+
       normalize_token
       
-      print '<<<<<<<<<<<<<<<<<<<<<<<<< token ' << params[:token]
       @token = OauthToken.where(token: params[:token]).all_in(scope: [action]).first
-      
-      print '<<<<<<<<<<<<<<<<<<<<<<<<< ' << @token.inspect
       
       if @token.nil? or @token.blocked?
         render text: "Unauthorized access.", status: 401
@@ -71,22 +64,15 @@ class ApplicationController < ActionController::Base
     end
 
     def normalize_token
-      
-      print '<<<<<<<<<<<<<<<<<<<<<< normalize\n'
       # Token in the body
       if (@body and @body[:token] and json_body)
-        print '<<<<<<<<<<<<<<<<<<<<<< json esta no body'
         params[:token] = @body[:token]
       end
       
-      print '<<<<<<<<<<<<<<<<<<<<<< depois da verificacao do body'
       # Token in the header
       if request.env["Authorization"]
-        print '<<<<<<<<<<<<<<<<<<<<<< json esta no authorization\n'
         params[:token] = request.env["Authorization"].split(" ").last
       end
-      
-      print '<<<<<<<<<<<<<<<<<<<<<< normalize_token'
     end
 
     def admin_does_not_exist
